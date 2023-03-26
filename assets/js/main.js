875f940156c8c4e2181f4624477da55650dea501
Jethro <jethrotiago@hotmail.com>
2023-03-26 20:57:46 -0300

    Corrigindo bug no id de formulário

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,19 @@
 let listaDeCompras = [];
 
-const formulario = document.querySelector('formulario');
-const itensInput = document.querySelector('receber-item');
+const formulario = document.getElementById("formulario-itens");
+const itensInput = document.getElementById("receber-item");
 
-formulario.addEventListener("submit", function(evento) {
+formulario.addEventListener("submit", function (evento) {
     evento.preventDefault();
+    salvarItem();
 });
 
 function salvarItem() {
     const comprasItem = itensInput.value
     
-    listaDeCompras = {
+    listaDeCompras.push ({
         valor: comprasItem
-    }
+    });
+
+    console.log(listaDeCompras);
 }
