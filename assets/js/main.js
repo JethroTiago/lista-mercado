9324219c2b3f0c8d8a9e379625fa87ae586a705a
Jethro <jethrotiago@hotmail.com>
2023-03-26 20:35:58 -0300

    Adicionando variaveis com queryselector

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,5 @@
 let listaDeCompras = [];
+
+const formulario = document.querySelector('formulario');
+const campoDeEscrita = document.querySelector('receber-item');
+const botaoSalvar = document.querySelector('botao');
