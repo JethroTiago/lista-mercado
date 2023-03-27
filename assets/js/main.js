cdcd478e9e84e713c05d7e6509129e6bec004b42
Jethro <jethrotiago@hotmail.com>
2023-03-27 15:54:21 -0300

    Adicionado códigos da li

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,12 @@ let listaDeCompras = [];
 
 const formulario = document.getElementById("formulario-itens");
 const itensInput = document.getElementById("receber-item");
+const ulItens = document.getElementById("lista-itens");
 
 formulario.addEventListener("submit", function (evento) {
     evento.preventDefault();
     salvarItem();
+    exibeItens();   
 });
 
 function salvarItem() {
@@ -21,4 +23,21 @@ function salvarItem() {
     }
     
     console.log(listaDeCompras);
+}
+
+function exibeItens() {
+    ulItens.innerHTML = "";
+    listaDeCompras.forEach((elemento, indice) => {
+        ulItens.innerHTML += `
+            <li class="item-compra" data-value="${indice}">
+                <div>
+                    <input type="checkbox" class="clicavel" />
+                    <input type="text" class="size" value="${elemento.valor}"></input>
+                </div>
+                <div>
+                    <i class="deletar"></i>
+                </div>
+            </li>
+        `
+    })
 }
